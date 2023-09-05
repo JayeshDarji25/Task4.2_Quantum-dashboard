a3556736985dab365a96be9c1a35cf33f7253566
Jayesh Darji <jayesh.darji@zeuslearning.mumbai>
2023-09-05 11:00:41 +0530

    second commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+let item: HTMLDivElement = document.getElementById("items") as HTMLDivElement;
+let it: HTMLCollectionOf<Element> = document.getElementsByClassName("itm") as HTMLCollectionOf<Element>;
+
+for(let i=0;i<it.length;i++){
+  itm[i]?.addEventListener("click", function handleClick(event) {
+    let current = document.getElementsByClassName("active");
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
