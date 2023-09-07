36d9ad99b1baeacfad888fa28a88ed6d780d3cf6
Jayesh Darji <jayesh.darji@zeuslearning.mumbai>
2023-09-07 09:54:48 +0530

    changes commit

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ let item: HTMLDivElement = document.getElementById("items") as HTMLDivElement;
 let it: HTMLCollectionOf<Element> = document.getElementsByClassName("itm") as HTMLCollectionOf<Element>;
 
 for(let i=0;i<it.length;i++){
-  itm[i]?.addEventListener("click", function handleClick(event) {
+  it[i]?.addEventListener("click", function handleClick(event) {
     let current = document.getElementsByClassName("active");
     if (current.length > 0) { 
       current[0].className = current[0].className.replace(" active", "");
